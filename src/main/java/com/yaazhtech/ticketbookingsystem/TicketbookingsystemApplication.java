30f03bf41b85699ef65a2cbc33fe9c862ee1cd1f
PreethiRG <hr@yaazhtech.com>
2025-10-27 13:31:40 +0530

    commit

---
+++ src/main/java/com/yaazhtech/ticketbookingsystem/TicketbookingsystemApplication.java
@@ -0,0 +1,13 @@
+package com.yaazhtech.ticketbookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketbookingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketbookingsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yaazhtech/ticketbookingsystem/TicketbookingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaazhtech.ticketbookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketbookingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
