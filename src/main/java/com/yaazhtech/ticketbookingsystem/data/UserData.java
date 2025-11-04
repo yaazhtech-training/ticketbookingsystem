6e0f3495dc4f3e03c8335d05638e4091ab52226a
kannanyaazhetech <sathyanatu97@gmail.com>
2025-11-04 12:49:19 +0530

    user

--- src/main/java/com/yaazhtech/ticketbookingsystem/data/UserData.java
+++ src/main/java/com/yaazhtech/ticketbookingsystem/data/UserData.java
@@ -1,6 +1,15 @@
 package com.yaazhtech.ticketbookingsystem.data;
 
+import jakarta.persistence.*;
+
+import javax.lang.model.element.Name;
+
+@Entity
+@Table(name="users")
+
 public class UserData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long userId;
     private String movieName;
     private String theatreName;
@@ -8,4 +17,60 @@ public class UserData {
     private String showTime;
     private double amount;
 
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public String getTheatreName() {
+        return theatreName;
+    }
+
+    public void setTheatreName(String theatreName) {
+        this.theatreName = theatreName;
+    }
+
+    public String getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(String seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public String getShowTime() {
+        return showTime;
+    }
+
+    public void setShowTime(String showTime) {
+        this.showTime = showTime;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public UserData(Long userId, String movieName, String theatreName, String seatNumber, String showTime, double amount) {
+        this.userId = userId;
+        this.movieName = movieName;
+        this.theatreName = theatreName;
+        this.seatNumber = seatNumber;
+        this.showTime = showTime;
+        this.amount = amount;
+    }
 }
--- src/main/java/com/yaazhtech/ticketbookingsystem/model/UserId.java
+++ src/main/java/com/yaazhtech/ticketbookingsystem/model/UserId.java
@@ -8,5 +8,60 @@ public class UserId {
     private String showTime;
     private double amount;
 
+    public Long getUserId() {
+        return userId;
+    }
 
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public String getTheatreName() {
+        return theatreName;
+    }
+
+    public void setTheatreName(String theatreName) {
+        this.theatreName = theatreName;
+    }
+
+    public String getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(String seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public String getShowTime() {
+        return showTime;
+    }
+
+    public void setShowTime(String showTime) {
+        this.showTime = showTime;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public UserId(Long userId, String movieName, String theatreName, String seatNumber, String showTime, double amount) {
+        this.userId = userId;
+        this.movieName = movieName;
+        this.theatreName = theatreName;
+        this.seatNumber = seatNumber;
+        this.showTime = showTime;
+        this.amount = amount;
+    }
 }
